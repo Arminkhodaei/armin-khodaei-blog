669ce8a4f3d570edde4f1510966e9fac435bbb5e
Arminkhodaei <Arminkhodaei@gmail.com>
2019-05-03 23:15:26 +0430

    GraphQl and Apollo Cilent are added to the poroject

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,30 @@
-import React from 'react';
-
+import React, { useEffect } from 'react';
+import { gql } from 'apollo-boost';
+import client from './lib/init-apollo';
 import Layout from '../layouts/mainLayout';
 
-const App = () => (
-  <Layout>
-    <p>Main page content</p>
-  </Layout>
-);
+const App = () => {
+  useEffect(() => {
+    client({})
+      .query({
+        query: gql`
+          query {
+            allUsers {
+              id
+              firstName
+              lastName
+            }
+          }
+        `
+      })
+      .then(result => console.log(result));
+  }, []);
+
+  return (
+    <Layout>
+      <p>Main page content</p>
+    </Layout>
+  );
+};
 
 export default App;
